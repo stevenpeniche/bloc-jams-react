9b59c85a2a944f1a49e89a870455eadd673ff7c0
Steven Peniche <stevenmpeniche@gmail.com>
2017-12-26 21:22:04 -0500

    Add 'Landing, Library, Album' components

---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+	<section className='album'>
+		Albums are going to live here
+	</section>
+);
+
+export default Album;
---
+++ src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Library = () => (
+	<section className="library">
+		Ayo it's the library page
+	</section>
+);
+
+export default Library;
