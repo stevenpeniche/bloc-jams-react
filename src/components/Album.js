0c32a1a11f5aeaf7b7f609e311723a88fd1132d9
Steven Peniche <stevenmpeniche@gmail.com>
2017-12-26 22:38:23 -0500

    Add 'album data & :slug routes'

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-	<section className='album'>
-		Albums are going to live here
-	</section>
-);
+class Album extends Component {
+	render() {
+		return (
+			<section className='album'>
+				{this.props.match.params.slug} Album will go here
+			</section>
+		);
+	}
+}
 
 export default Album;
